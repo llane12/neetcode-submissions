15f04760145f4740326a8652397c078872da6fd8
Luke Lane <lukelane1@gmail.com>
2026-04-08 14:26:41 +0900

    Add: counting-bits - submission-1

---
+++ Data Structures & Algorithms/counting-bits/submission-1.go
@@ -0,0 +1,13 @@
+func countBits(n int) []int {
+	res := make([]int, n+1)
+	res[0] = 0
+
+	offset := 1
+	for i := 1; i <= n; i++ {
+		if offset * 2 == i {
+			offset = i
+		}
+		res[i] = 1 + res[i - offset]
+	}
+	return res
+}
