ef4e6aa5e4d4bfebb27baae3cf58a994fd27c7b2
Luke Lane <lukelane1@gmail.com>
2026-04-16 09:33:44 +0900

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.go
@@ -0,0 +1,17 @@
+// Dynamic Programming (Bottom-Up)
+// Time complexity: O(n)
+// Space complexity: O(n)
+
+func climbStairs(n int) int {
+    if n <= 2 {
+		return n
+	}
+
+	dp := make([]int, n+1)
+	dp[1] = 1
+	dp[2] = 2
+	for i := 3; i <= n; i++ {
+        dp[i] = dp[i - 1] + dp[i - 2]
+    }
+    return dp[n]
+}
